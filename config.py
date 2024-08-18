79968a84a16e2799bffb05fc872169ba34fa19ac
combe4259 <82356080+combe4259@users.noreply.github.com>
2024-08-19 02:27:17 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ print("Config 모듈이 로드되었습니다")
 CLIENT_ID = "d28959f1c6ade7c33f4655a05e406d6d"
 CLIENT_SECRET = "JLFPTmKUwfIFxL24QKJVvXaeZt9olFqV"
 REDIRECT_URI = "http://127.0.0.1:5000/oauth"
-MYSQL_DATABASE_URI = 'mysql+pymysql://combe4259:mysql4259@localhost/db_name'
+MYSQL_DATABASE_URI = 'mysql+pymysql://inter4259:mysql4259@localhost/db_name'
